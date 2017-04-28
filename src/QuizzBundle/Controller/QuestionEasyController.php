c56862bb12fae9855a3464082543b1a7147cdc27
Fanny-Jan <fanny.jan12@gmail.com>
2017-04-28 10:09:32 +0200

    tvqkcbsclb

--- src/QuizzBundle/Controller/QuestionEasyController.php
+++ src/QuizzBundle/Controller/QuestionEasyController.php
@@ -5,7 +5,8 @@ namespace QuizzBundle\Controller;
 use QuizzBundle\Entity\QuestionEasy;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Questioneasy controller.
